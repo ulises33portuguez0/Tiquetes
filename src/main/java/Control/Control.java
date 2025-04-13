daa8f3fc57617171f8c3ad0bd4f9db0753d1b595
ulises33portuguez0 <uportuguezm@ucenfotec.ac.cr>
2025-04-12 19:43:41 -0600

    carga inicial package control

---
+++ src/main/java/Control/Control.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Control;
+
+/**
+ *
+ * @author ulise
+ * Los usuarios creadores solo pueden crear tiquetes y consultar tiquetes. 
+Los usuarios técnicos pueden consultar tiquetes y modificar los mismos, 
+incluyendo su estado.
+* https://github.com/ulises33portuguez0/Tiquetes.git
+ */
+public class Control {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
